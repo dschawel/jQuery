4f70edc1b9de6e96fc7e47fb9ec3d3eaccf2e363
dschawel <david.schawel@gmail.com>
2020-10-26 13:35:58 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    console.log($)
+    const $div = $('<div>')
+    // console.log($div)
+    $('body').append( $div )
+    // console.log($('body'))
+    $div.addClass('meat')
+
+    const $h3 = $('<h3>').text('Ostrich')
+    $div.append($h3)
+
+    const $newDiv = $('<div>')
+    $newDiv.addClass('dairy')
+    const $newh3 = $('<h3>').text('Soy Milk')
+    $('body').append($newDiv)
+    $newDiv.append($newh3)
+
+    const $divs = $('div')
+    console.log($divs)
+    // $divs.hide()
+    const $meat = $('.meat')
+    // $meat.hide()
+
+    const $img = $('<img>', {src: 'https://images.unsplash.com/photo-1452195100486-9cc805987862?auto=format&fit=crop&w=750&q=60&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D', alt: 'cheese'})
+    $('body').prepend($img)
+
+    console.log($divs.eq(2).children())
+    $divs.eq(2).children().text('Kohlrabi')
+
+    $('body').css("border", "1em solid black")
+})
